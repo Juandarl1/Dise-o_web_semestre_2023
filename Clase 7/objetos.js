3a1a041508124012abf7cff46eb717de7ee8e8f7
Juan David Rodriguez <juandavidrodriguezlondono@gmail.com>
2023-03-22 11:13:06 -0500

    Actualización
    Hecho en clase

---
+++ Clase 7/objetos.js
@@ -0,0 +1,94 @@
+//obejtos
+const persona={
+    primerNombre:'Juan',
+    apellido:'Perez',
+    edad:22,
+    documentoIdentidad: 5612,
+    caracteristica:{
+        colorCabello:'rojo',
+        etnicidad:'Indouropeo',
+        idiomaMaterno:'Español'
+    },
+
+    //Método
+    saludar:function(){
+        return `hola ${this.primerNombre} ${this.apellido}`
+    }
+};
+//accediendo al método
+console.log(persona.saludar());
+//acceder a una propiedad. Forma 1
+console.log(persona.apellido);
+//acceder a una propiedad. Segunda forma
+console.log(persona['primerNombre']);
+
+console.log(persona);
+
+
+//clases
+
+class Persona {
+    constructor(primerNombreUsuario,apellidoUsuario){
+        this.primerNombreUsuario=primerNombreUsuario;
+        this.apellidoUsuario=apellidoUsuario;
+    }
+    //método
+    saludarPersona(){
+        return `Hola ${this.primerNombreUsuario} ${this.apellidoUsuario}`
+    }
+}
+
+//voy a usar la clase para construir una persona (objeto)
+let personita=new Persona('Juan','David');
+personita.primerNombreUsuario='Carlos';
+personita.apellidoUsuario='Cantor';
+console.log(personita);
+console.log(personita.saludarPersona());
+
+//Herencia
+class animales{
+    constructor(animal1,edad,sonido){
+        this.animal1=animal1;
+        this.edad=edad;
+        this.sonido=sonido;
+    }
+    //métodos
+    sonido(){
+        return `${this.animal1} tiene el sonido: ${this.sonido}`;
+
+        }
+}
+
+//HERENCIA palabra clave extends
+class Gatos extends animales{
+    constructor(animal1,edad,sonido,caza){
+        super(animal1,edad,sonido);
+        this.caza=caza;
+    }
+    //método
+    maullar(){
+        return `Yo puedo hacer el sonido ${this.sonido} `;
+    }
+}
+
+//crear un animal, especificamente
+let gato=new Gatos('misifus',52,'Meow',false);
+console.log(gato);
+console.log(gato.maullar());
+
+
+//Tarea
+
+/* 
+1. Crear un objeto que tenga mínimo dos metodos
+    ademas debe tener una propiedad que sea un objeto.
+
+2. Crear una clase que herede de otra.
+    debe poseer mínimo 3 propiedades para el constructor.
+
+
+3. Crear una función tipo flecha que haga uso de la clase
+    construida y el objeto declarado.
+
+
+*/ 
