3c7bad7bb58105048c4f49ecbbdc2f422c9cc74e
Juan David Rodriguez <juandavidrodriguezlondono@gmail.com>
2023-03-29 11:43:18 -0500

    update

---
+++ Clase 8/module2.js
@@ -0,0 +1,8 @@
+// export default, Significa que por defecto exporta la función
+// la puedo llamar al otro módulo
+
+export const sumar = (numero1,numero2)=>{
+    return numero1+numero2;
+}
+
+export const nombre='juan perez';
---
+++ Clase 8/modulos.js
@@ -0,0 +1,29 @@
+//Modulos en JavaScript
+import {sumar,nombre} from './module2.js';
+
+
+console.log(nombre);
+
+console.log(sumar(5,3));
+
+
+/*
+    Crear minimo dos modulos.
+
+    el primer modulo debe tener:
+    una clase que hereda de otra
+    un objeto que dentro de sus propiedades tenga otro objeto
+
+    el segundo modulo debe tener:
+
+    importacion de la clase y el objeto
+    funcion tipo flecha que realice el uso de la clase y objeto importados
+
+    oros adicionales:
+
+    Tercer modulo que contenga:
+    una funcion tipo flecha, que manipule arreglos con los métodos de arreglos
+    debe tener como parametros de entrada el arreglo y una callback function
+    debe poderse exportar.
+
+*/
